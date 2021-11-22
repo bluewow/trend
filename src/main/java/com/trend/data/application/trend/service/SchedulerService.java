abde905702dafba22ecfece5738db0ed37e6bea8
bluewow <dogseen@gmail.com>
2021-11-23 05:47:22 +0900

    feat : 매일 오후 5시 데이터 수집, 6시 메시지 전달

--- src/main/java/com/trend/data/application/trend/service/SchedulerService.java
+++ src/main/java/com/trend/data/application/trend/service/SchedulerService.java
@@ -1,4 +1,4 @@
-package com.trend.data.application.trend.infra;
+package com.trend.data.application.trend.service;
 
 import com.trend.data.application.trend.domain.TrendModel;
 import com.trend.data.application.trend.service.Rss;
@@ -9,24 +9,37 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 @PropertySource("classpath:constant.properties")
 @RequiredArgsConstructor
 @Component
-public class Scheduler {
+public class SchedulerService {
 
     private final Rss rss;
 
     @Value("${google.trend.url}")
     private String googleURL;
 
-    @Scheduled(fixedDelay = 1000)
+    private List<TrendModel> data = new ArrayList<>();
+
+    /**
+     * 매일 오후 5시 데이터를 수집한다
+     */
+    @Scheduled(cron = "0 0 17 ? * * *")
     @Async
     public void getDataTask() {
         List<TrendModel> trendModels = rss.reader(googleURL);
-        for (TrendModel trendModel : trendModels) {
-            System.out.println(trendModel.toString());
-        }
+        data = trendModels;
     }
+
+    /**
+     * 매일 오후 6시 수집한 데이터를 토대로 메시지를 전달한다
+     */
+    @Scheduled(fixedDelay = 1000)
+    @Async
+    public void messagePushTask() {
+        System.out.println(data.toString());
+    }
+
 }
