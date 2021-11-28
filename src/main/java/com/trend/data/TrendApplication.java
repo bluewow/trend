68a0146db379c66155f3c3ffd6c3646c8f1ae3fb
bluewow <dogseen@gmail.com>
2021-11-28 23:16:31 +0900

    test : webhook test

--- src/main/java/com/trend/data/TrendApplication.java
+++ src/main/java/com/trend/data/TrendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrendApplication {
-sdf
+
     public static void main(String[] args) {
         SpringApplication.run(TrendApplication.class, args);
     }
